4987e4e390703ae541428d47db5ef33f112d931a
Martin Contento <martin.contento@gameforge.com>
2011-04-20 23:35:27 +0200

    moved classes to Loca subdir

--- public/Bootstrap.php
+++ public/Bootstrap.php
@@ -5,7 +5,7 @@ $config['db_path'] = dirname(__FILE__) . '/../data/';
 $config['db_database'] = 'test123';
 
 // TODO maybe handle this with an autoloader?
-require_once dirname(__FILE__) . '/../library/Database/Sqlite.php'
+require_once dirname(__FILE__) . '/../library/Loca/Database/Sqlite.php'
 
 $registry = array();
-$registry['database'] = new Database_Sqlite($config);
+$registry['database'] = new Loca_Database_Sqlite($config);
