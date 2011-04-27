005274e80e1dc23100fb7a2b5f2da66ec0cc7b44
Tinnet Coronam <tinnet@coronam.net>
2011-04-27 20:13:21 +0200

    added stub tmx exporter

---
+++ library/Loca/Exporter/Tmx.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * TODO!
+ *
+ * format:
+ *
+ * <?XML VERSION="1.0" ENCODING="UTF-8" ?>
+ * <TMX VERSION="1.0" >
+ *    <HEADER></HEADER>
+ * </TMX
+ */
+class Loca_Exporter_Tmx {
+    public function __construct($config)
+    {
+        throw new Exception('not done yet');
+    }
+
+    public function export($project, $keys)
+    {
+        throw new Exception('not done yet');
+    }
+
+    public function getHeaders()
+    {
+        return array('Content-type: text/xml');
+    }
+}
